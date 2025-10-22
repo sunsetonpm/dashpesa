71d24b14072b0b9fbba55227a9bea0cafd2ddee9
Calton Korir <kcaltoncheruiyot@gmail.com>
2025-10-22 23:02:46 +0300

    Store fix

--- api/index.php
+++ api/index.php
@@ -183,7 +183,7 @@ if ($page == 'process_payment' && $_SERVER["REQUEST_METHOD"] == "POST") {
 
     $consumerKey = "qN3VPTVG7wd3hiWrntEU51GnGhbAtQlQShhoDmNOxilFyMIE";
     $consumerSecret = "sMGE5qDKAUtSbYj4oEK6hG2KZiNiYG3m4n5wYqtGaGtdsAoDbgz8kxTWppxy5gBj";
-    $mpesaShortCode = "4461850";
+    $mpesaShortCode = "5003032";
     $mpesaPasskey = "4df10de01e5a7a3aef9fdb44741e4eca6f272d70dd0f59326cd8e4e824a39625";
     $callbackUrl = "https://dashpesa.vercel.app/callback.php";
     $environment = "live";
